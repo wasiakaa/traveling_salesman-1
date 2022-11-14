097dc036dedfc7597ac731879b3e5c9725697659
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2022-11-14 11:24:47 +0100

    algorytm bez cyklu Hamiltona

--- christofides.py
+++ christofides.py
@@ -1,9 +1,25 @@
-MST = kruskal(G)
+from kruskal import *
+from graph import *
+from min_match import *
+import networkx as nx
 
-M = min_match() 'na wierzchołkach nieparzystego stopnia'
+def christofides(G):
+    MST = kruskal(G)
 
-'Multigraf= MST + M'
+    degs = nx.degree(MST.to_nx())
+    n = len(degs)
+    MST_odd = list()
 
-E = nx.eulerian_circuit(Multigraf)
+    for i in range(n):
+        if degs[i][1]%2 != 0:
+            MST_odd.append(degs[i][0])
+        return MST_odd
 
-'H = cykl Hamiltona z E'
+    G_odd = nx.subgraph(G.to_nx(), MST_odd)
+    M = min_match(G_odd)
+
+    Multigraf = nx.union(MST,M)
+
+    E = nx.eulerian_circuit(Multigraf)
+
+    'H = cykl Hamiltona z E'
