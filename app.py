023e0f7b0bd3c61a27b8b783d6c69854c9640a14
Milosz22 <72401562+Milosz22@users.noreply.github.com>
2022-11-21 11:15:59 +0100

    Dodałem wyswietl i button3
    Zrobiłem guzik, który wyświetla graf.

--- app.py
+++ app.py
@@ -16,5 +16,16 @@ myButton1 = Button(root,text="Kliknij mnie  😳",padx=50,pady=50,font=50,comman
 myButton2 = Button(root,text="Zapisz",padx=50,pady=50,font=50,command=save)
 myButton1.grid(row=1)
 myButton2.grid(row=2)
+
+def wyswietl():
+    tajp=nx.empty_graph()
+    if (type(G)==type(tajp)):
+        plt.show()
+        nx.draw(G)
+
+    return
+myButton3 = Button(root,text="Wyswietl",padx=p,pady=p,font=50,command=wyswietl)
+myButton3.grid(row=3)
+
 root.mainloop()
 
