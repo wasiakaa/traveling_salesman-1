b431f418e87ba3d0ac7de8f62df6b629a7a1e8e7
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2022-11-26 16:59:30 +0100

    Naprawiłam funkcję min_match

--- min_match.py
+++ min_match.py
@@ -1,9 +1,6 @@
 from graph import *
 import networkx as nx
-import networkx.algorithms.matching as matching
 
-import networkx as nx
+
 def min_match(Graf):
-    return nx.min_weight_matching(Graf.to_nx())
-
-
+    return nx.min_weight_matching(graph(Graf).to_nx())
