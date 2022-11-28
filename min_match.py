a7d641bc358581ae2a2014de3e967d8b6adcd6b4
wasiakaa <115791012+wasiakaa@users.noreply.github.com>
2022-11-28 11:44:08 +0100

    Usunęłam plik min_match.py

--- min_match.py
+++
@@ -1,7 +0,0 @@
-from graph import *
-import networkx as nx
-
-
-def min_match(G):
-    '''Returns a set of edges that appear in minimum-weight perfect matching'''
-    return nx.min_weight_matching(graph(G).to_nx())
