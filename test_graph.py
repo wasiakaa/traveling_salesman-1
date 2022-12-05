79f3f426db243410849cd64e8cf28fdb16cc9359
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2022-12-05 10:37:33 +0100

    Poprawiłam czystość kodu

--- test_graph.py
+++ test_graph.py
@@ -1,5 +1,4 @@
 import unittest
-import networkx as nx
 from graph import *
 
 
@@ -9,8 +8,8 @@ class MyTestCase(unittest.TestCase):
         # Given
         G1 = nx.path_graph(4)
         for i in range(3):
-            G1[i+1][i]["weight"]=i*i+1
-        G1_graph=graph(G1)
+            G1[i + 1][i]["weight"] = i * i + 1
+        G1_graph = graph(G1)
         expected_result = {"0": {"1": 1}, "1": {"0": 1, "2": 2}, "2": {"1": 2, "3": 5}, "3": {"2": 5}}
 
         # When
@@ -18,13 +17,14 @@ class MyTestCase(unittest.TestCase):
 
         # Then
         self.assertEqual(expected_result, result)
+
     def test_init_matrix(self):
         # Given
-        G1=[[0, 1, 0, 0],
-            [1, 0, 2, 0],
-            [0, 2, 0, 5],
-            [0, 0, 5, 0]]
-        G1_graph=graph(G1)
+        G1 = [[0, 1, 0, 0],
+              [1, 0, 2, 0],
+              [0, 2, 0, 5],
+              [0, 0, 5, 0]]
+        G1_graph = graph(G1)
         expected_result = {0: {1: 1}, 1: {0: 1, 2: 2}, 2: {1: 2, 3: 5}, 3: {2: 5}}
 
         # When
@@ -32,5 +32,7 @@ class MyTestCase(unittest.TestCase):
 
         # Then
         self.assertEqual(expected_result, result)
+
+
 if __name__ == '__graph__':
     unittest.graph()
