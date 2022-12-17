bd51942bdbaa4f3eeacdc5f12379fbf574d5b816
sabinamajewska <115459150+sabinamajewska@users.noreply.github.com>
2022-12-17 23:33:10 +0100

    Usunęłam test_load 1-3

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -4,39 +4,39 @@ from Christofides.christofides import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_load_graph_from_file_1(self):
-        # Given
-        filepath = "test_load_1.txt"
-        expected_result = {"0": {"1": 1, "2": 2}, "1": {"0": 1, "2": 2}, "2": {"0": 2, "1": 2}}
+#     def test_load_graph_from_file_1(self):
+#         # Given
+#         filepath = "test_load_1.txt"
+#         expected_result = {"0": {"1": 1, "2": 2}, "1": {"0": 1, "2": 2}, "2": {"0": 2, "1": 2}}
 
-        # When
-        result = load_graph_from_file(filepath)
+#         # When
+#         result = load_graph_from_file(filepath)
 
-        # Then
-        self.assertEqual(expected_result, result)
+#         # Then
+#         self.assertEqual(expected_result, result)
 
-    def test_load_graph_from_file_2(self):
-        # Given
-        filepath = "test_load_2.txt"
-        expected_result = {"0": {"1": 1, "2": 2, "3": 3}, "1": {"0": 1, "2": 2, "3": 3}, "2": {"0": 2, "1": 2, "3": 3},
-                           "3": {"0": 3, "1": 3, "2": 3}}
+#     def test_load_graph_from_file_2(self):
+#         # Given
+#         filepath = "test_load_2.txt"
+#         expected_result = {"0": {"1": 1, "2": 2, "3": 3}, "1": {"0": 1, "2": 2, "3": 3}, "2": {"0": 2, "1": 2, "3": 3},
+#                            "3": {"0": 3, "1": 3, "2": 3}}
 
-        # When
-        result = load_graph_from_file(filepath)
+#         # When
+#         result = load_graph_from_file(filepath)
 
-        # Then
-        self.assertEqual(expected_result, result)
+#         # Then
+#         self.assertEqual(expected_result, result)
 
-    def test_load_graph_from_file_3(self):
-        # Given
-        filepath = "test_load_3.txt"
-        expected_result = {"0": {"1": 1}, "1": {"0": 1}}
+#     def test_load_graph_from_file_3(self):
+#         # Given
+#         filepath = "test_load_3.txt"
+#         expected_result = {"0": {"1": 1}, "1": {"0": 1}}
 
-        # When
-        result = load_graph_from_file(filepath)
+#         # When
+#         result = load_graph_from_file(filepath)
 
-        # Then
-        self.assertEqual(expected_result, result)
+#         # Then
+#         self.assertEqual(expected_result, result)
 
     def test_save_christo_graph_to_file_1(self):
         # Given
